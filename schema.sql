973f76d2f9c047a7028328030831175b8b27f385
Diogo G <giomodiogo@gmail.com>
2018-10-05 19:03:33 -0300

    ajustes

--- schema.sql
+++ schema.sql
@@ -1,12 +1,31 @@
+DROP TABLE aluno;
+DROP TABLE cidade;
+
+CREATE TABLE cidade
+(
+    codigo INTEGER NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    PRIMARY KEY (codigo)
+);
+
 CREATE TABLE aluno
 (
     codigo INTEGER NOT NULL,
     nome VARCHAR(100) NOT NULL,
     ra VARCHAR(20) NOT NULL,
-    PRIMARY KEY (codigo)
-)
+    cod_cidade INT NOT NULL,
+    PRIMARY KEY (codigo),
+    FOREIGN KEY (cod_cidade) REFERENCES cidade (codigo)
+);
 
 
-INSERT INTO aluno VALUES (1, "Ana", 123456);
-INSERT INTO aluno VALUES (2, "Joao", 234567);
-INSERT INTO aluno VALUES (3, "Pedro", 654321);
+INSERT INTO cidade VALUES (1, "Cascavel");
+INSERT INTO cidade VALUES (2, "Cafelândia");
+
+
+INSERT INTO aluno VALUES (1, "Ana", 123456, 1);
+INSERT INTO aluno VALUES (2, "Joao", 234567, 1);
+INSERT INTO aluno VALUES (3, "Pedro", 654321, 2);
+
+
+UPDATE aluno SET cod_cidade = 1 WHERE ra = 123456;
--- src/main/java/org/funtimecoding/java/mysql/example/ExampleMain.java
+++ src/main/java/org/funtimecoding/java/mysql/example/ExampleMain.java
@@ -12,7 +12,7 @@ public class ExampleMain {
 	static String HOST = "localhost";
 	static String PORT = "3306";
 	static String USER = "root";
-	static String PASS = "univel";
+	static String PASS = "1234qwer";
 	// static String PASS = "univel";
 	static String DATA_BASE = "example_java_mysql";
 
@@ -35,15 +35,17 @@ public class ExampleMain {
 			Connection connection = DriverManager.getConnection(connectionURL, USER, PASS);
 
 			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM aluno");
+			ResultSet resultSet = statement.executeQuery("SELECT a.ra, a.nome, c.nome as cidade FROM aluno a "
+					+ "INNER JOIN cidade c ON (a.cod_cidade = c.codigo);");
 
 			StringBuilder sb = new StringBuilder();
 			while (resultSet.next()) {
-				String codigo = resultSet.getString("codigo");
 				String nome = resultSet.getString("nome");
 				String ra = resultSet.getString("ra");
+				String cidade = resultSet.getString("cidade");
 
-				sb.append(codigo).append(" | ").append(nome).append(" | ").append(ra).append("\n");
+				sb.append("RA: ").append(ra).append("\n").append("Nome: ").append(nome).append("\n").append("Cidade: ")
+						.append(cidade).append("\n\n");
 			}
 
 			JOptionPane.showMessageDialog(null, sb.toString());
